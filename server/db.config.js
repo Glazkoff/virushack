476157b32f4c399c6a6d5925e8b64a1c36c0efc7
Nikita Glazkov <zitrnik@gmail.com>
2020-05-02 22:07:35 +0300

    DEV: Добавил шаблон серверной части

---
+++ server/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "VH286.spaceweb.ru",
+  USER: "polywebage",
+  PASSWORD: "Polyweb2020",
+  DB: "polywebage"
+};
---
+++ server/server.js
@@ -0,0 +1,65 @@
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const path = require("path");
+const dbConfig = require("./db.config.js");
+
+const app = express();
+
+// Парсинг json - application/json
+app.use(bodyParser.json());
+
+// Парсинг запросов по типу: application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// Обработка статических файлов
+app.use("/", serveStatic(path.join(__dirname, "../dist/virushack")));
+
+// настройка CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// создаем соединение с нашей базой данных
+// const connection = mysql.createPool({
+//   connectionLimit: 10,
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+//   charset: "utf8_general_ci",
+// });
+
+// connection.getConnection((err, connection) => {
+//   if (err) {
+//     if (err.code === "PROTOCOL_CONNECTION_LOST") {
+//       console.error("Database connection was closed.");
+//     }
+//     if (err.code === "ER_CON_COUNT_ERROR") {
+//       console.error("Database has too many connections.");
+//     }
+//     if (err.code === "ECONNREFUSED") {
+//       console.error("Database connection was refused.");
+//     }
+//   } else {
+//     connection.query('SET NAMES "utf8"');
+//     connection.query('SET CHARACTER SET "utf8"');
+//     connection.query('SET SESSION collation_connection = "utf8_general_ci"');
+//     console.log("Успешно соединено с БД");
+//   }
+//   if (connection) connection.release();
+// });
